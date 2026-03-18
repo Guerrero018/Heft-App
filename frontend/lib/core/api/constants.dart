a0f1dd822e2e69d819b6086098f4570fc78c654a
Guerrero018 <izanguer@gmail.com>
2026-03-18 22:00:02 +0100

    Update API URL to production

--- frontend/lib/core/api/constants.dart
+++ frontend/lib/core/api/constants.dart
@@ -1,7 +1,6 @@
 class AppConstants {
-  // Use 10.0.2.2 for Android Emulator to connect to localhost Django
-  // Use http://127.0.0.1:8000 for iOS Simulator
-  static const String baseUrl = 'http://10.0.2.2:8000/api/';
+  // Cambiamos la IP local por tu URL de Render (IMPORTANTE: Debe terminar en /api/)
+  static const String baseUrl = 'https://heft-backend-ywi0.onrender.com/api/';
 
   static const String tokenKey = 'heft_access_token';
   static const String refreshTokenKey = 'heft_refresh_token';
